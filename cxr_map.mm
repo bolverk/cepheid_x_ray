ab8f1273fc43e3fbd8b18ca882b9dd39d556fc8a
Bolverk <almog.yalin@gmail.com>
2017-11-08 11:13:17 -0500

    add link to resonant absorption model

--- cxr_map.mm
+++ cxr_map.mm
@@ -7,11 +7,15 @@
 </node>
 <node CREATED="1505140059064" ID="Freemind_Link_119319135" MODIFIED="1505140069963" POSITION="left" TEXT="Theoretical models">
 <node CREATED="1505140070824" ID="Freemind_Link_1622915205" MODIFIED="1505140120970" TEXT="Shock breakout?">
-<node CREATED="1505140075864" ID="Freemind_Link_58001003" MODIFIED="1505140093027" TEXT="Too much energy, an temperature too low"/>
+<node CREATED="1505140075864" ID="Freemind_Link_58001003" MODIFIED="1510157546628" TEXT="Too much energy, and temperature too low"/>
 </node>
 <node CREATED="1505140094519" ID="Freemind_Link_324756347" MODIFIED="1505140107962" TEXT="Variation in the Corona?">
-<node CREATED="1505251781957" ID="Freemind_Link_1891176358" MODIFIED="1505251788985" TEXT="Resonant absorption"/>
+<node CREATED="1510157404684" ID="Freemind_Link_1317251320" LINK="https://arxiv.org/pdf/1206.6097.pdf" MODIFIED="1510157422051" TEXT="Review paper ">
+<node CREATED="1505251781957" ID="Freemind_Link_1891176358" MODIFIED="1505251788985" TEXT="Resonant absorption">
+<node CREATED="1510157506889" ID="Freemind_Link_358537551" LINK="http://adsabs.harvard.edu/abs/1978ApJ...226..650I" MODIFIED="1510157514678" TEXT="Simplest analytic model"/>
+</node>
 <node CREATED="1505252180789" ID="Freemind_Link_607237473" MODIFIED="1505252183289" TEXT="nanoflares"/>
+</node>
 </node>
 </node>
 </node>
