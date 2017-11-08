0df6e078a71206a341ea35968893af8226f55b5d
Bolverk <almog.yalin@gmail.com>
2017-11-08 11:08:46 -0500

    add mind map

---
+++ cxr_map.mm
@@ -0,0 +1,18 @@
+<map version="0.8.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1505139241381" ID="Freemind_Link_165247685" MODIFIED="1505139252255" STYLE="bubble" TEXT="Cepheid x Ray">
+<node CREATED="1505139259131" ID="_" MODIFIED="1505139262236" POSITION="right" TEXT="Observation">
+<node CREATED="1505139382918" ID="Freemind_Link_1995750266" LINK="http://adsabs.harvard.edu/abs/2014ApJ...794...80E" MODIFIED="1505139393756" TEXT="XMM newton"/>
+<node CREATED="1505139396045" ID="Freemind_Link_1518901044" LINK="http://adsabs.harvard.edu/abs/2017ApJ...838...67E" MODIFIED="1505139411378" TEXT="Chandra"/>
+</node>
+<node CREATED="1505140059064" ID="Freemind_Link_119319135" MODIFIED="1505140069963" POSITION="left" TEXT="Theoretical models">
+<node CREATED="1505140070824" ID="Freemind_Link_1622915205" MODIFIED="1505140120970" TEXT="Shock breakout?">
+<node CREATED="1505140075864" ID="Freemind_Link_58001003" MODIFIED="1505140093027" TEXT="Too much energy, an temperature too low"/>
+</node>
+<node CREATED="1505140094519" ID="Freemind_Link_324756347" MODIFIED="1505140107962" TEXT="Variation in the Corona?">
+<node CREATED="1505251781957" ID="Freemind_Link_1891176358" MODIFIED="1505251788985" TEXT="Resonant absorption"/>
+<node CREATED="1505252180789" ID="Freemind_Link_607237473" MODIFIED="1505252183289" TEXT="nanoflares"/>
+</node>
+</node>
+</node>
+</map>
